ccefa1ad24af229738a6a69a1e5bfc0d32dd849a
Adam Walicki <awalicki04@gmail.com>
2025-11-20 15:08:16 +0100

    all requirements

--- PIP_Task2/simulation.py
+++ PIP_Task2/simulation.py
@@ -1,33 +1,71 @@
+import csv
+import json
 from time import sleep
 
-from wolf import Wolf
 from sheep import Sheep
+from wolf import Wolf
+
+
+def collect_round_data(round_no, wolf, sheeps):
+    wolf_pos = (wolf.place[0], wolf.place[1])
+    sheep_pos = []
+
+    for sheep in sheeps:
+        if sheep is not None:
+            sheep_pos.append((sheep.place[0], sheep.place[1]))
+        else:
+            sheep_pos.append(None)
+
+    return {
+        'round_no': round_no,
+        'wolf_pos': wolf_pos,
+        'sheep_pos': sheep_pos
+    }
+
+
+def save_to_json(data_list):
+    with open("pos.json", 'w') as file:
+        json.dump(data_list, file, indent=4)
+
+
+def save_to_csv(data_list):
+    with open("alive.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data_list)
 
 
 def num_sheeps_allive(sheeps):
-    sum = 0
+    sum_of_allive_sheeps = 0
     for i in range(len(sheeps)):
-        if sheeps[i] != None:
-            sum += 1
-    return sum
+        if sheeps[i] is not None:
+            sum_of_allive_sheeps += 1
+    return sum_of_allive_sheeps
+
 
 num_of_sheeps = 2
 max_num_of_rounds = 50
 
 wolf = Wolf()
 sheeps = []
+json_data = []
+csv_data = []
+
 for i in range(num_of_sheeps):
     sheeps.append(Sheep())
 
 for j in range(max_num_of_rounds):
     for i in range(num_of_sheeps):
-        if sheeps[i] != None:
+        if sheeps[i] is not None:
             sheeps[i].move()
 
     ch_sheep = wolf.move(sheeps)
 
+    round_data = collect_round_data(j + 1, wolf, sheeps)
+    json_data.append(round_data)
 
-    print(f"Round number: {j}")
+    csv_data.append([j + 1, num_sheeps_allive(sheeps)])
+
+    print(f"Round number: {j + 1}")
     print(wolf.get_place())
     print(f"Number of allive sheeps: {num_sheeps_allive(sheeps)}")
     if ch_sheep == -1:
@@ -43,3 +81,9 @@ for j in range(max_num_of_rounds):
     print(".\n.\n")
 
     sleep(1)
+
+save_to_json(json_data)
+print("Zapisano położenia zwierząt do pliku pos.json.")
+
+save_to_csv(csv_data)
+print("Zapisano liczbę żywych owiec do pliku alive.csv.")
